37602d043bba69808058384403248c307d7273c1
Parav Jaikrishan <r21947parav@dpsrkp.net>
2020-08-20 11:43:04 +0530

    Exercise 1 completed

--- Exercise1/index.js
+++ Exercise1/index.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 for(let filename = 1; filename <= 10; filename++) {
-    fs.readFile(path.join(__dirname, '/questions', `${filename}`), 'utf-8', function(err, body) {
+    fs.readFile(path.join(__dirname, '/questions', `${filename}`), 'utf-8', (err, body) => {
         if(err) throw err; 
         const questions = body.split("\n");
         fs.writeFile(path.join(__dirname, '/solutions', `${filename}`), '', err => {
             if (err) {
                 console.log("")
             }
-            questions.forEach((question) => {
+            questions.forEach(question => {
+                if(question === "") {
+                    fs.appendFile(path.join(__dirname, '/solutions', `${filename}`), "", err => {
+                        if (err) throw err;
+                    })
+                }
                 fs.appendFile(path.join(__dirname, '/solutions', `${filename}`), `${eval(question)}\n`, err => {
                     if (err) throw err;
                 })
